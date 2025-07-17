8370a745c7de58c2a049f0d4976c17eb2332e7b1
jeongminia <aim1016@naver.com>
2025-07-17 17:15:10 +0900

    fix: resolve all issues by downgrading Evidently to v0.5.1

--- dashboard/app_database/pages/upload_data.py
+++ dashboard/app_database/pages/upload_data.py
@@ -16,12 +16,13 @@ def render():
     st.session_state['dataset_name'] = dataset_name
     
     data_type = ['Text', 'Image', 'Tabular']
-    selected_data_type = st.selectbox("1. Choose data type:", 
+    selected_data_type = st.selectbox("Choose data type:", 
                                                  data_type)
     task_type = ['Classification', 'Regression']
-    selected_task_type = st.selectbox("2. Choose task type:",
+    selected_task_type = st.selectbox("Choose task type:",
                                                  task_type)
     st.subheader(f"{selected_data_type} Data Drift Detection for {selected_task_type}!")
     st.write("Please upload your train, validation, and test datasets.")
     
     uploaded = upload_and_store_data()
+    dataset_name = st.session_state['dataset_name']
